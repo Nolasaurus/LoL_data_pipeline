56a04d43320567b6a1a62c5fb36305259fbfd921
Nolan <meyer09@gmail.com>
2023-06-22 11:46:49 -0500

    incorporated get_API_key in all API functions

--- get_match_timeline.py
+++ get_match_timeline.py
@@ -1,8 +1,9 @@
 import requests
+from get_API_key import get_API_key
 
-def getMatchTimeline(match_id, API_key):
+def get_match_timeline(match_id):
     base_url = 'https://americas.api.riotgames.com/lol/match/v5/matches/{}/timeline?api_key={}'
-    url = base_url.format(match_id, API_key)
+    url = base_url.format(match_id, get_API_key())
     
     response = requests.get(url)
     
--- get_puuid_by_summon_id.py
+++ get_puuid_by_summon_id.py
@@ -1,8 +1,10 @@
 import requests
+from get_API_key import get_API_key
+
 # get PUU-id from summonerId
-def get_puuid_by_summon_id(summoner_id, API_key):
+def get_puuid_by_summon_id(summoner_id):
     base_url = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/{summoner_id}?api_key={API_key}'
-    url = base_url.format(API_key=API_key, summoner_id=summoner_id)
+    url = base_url.format(API_key=get_API_key(), summoner_id=summoner_id)
     
     response = requests.get(url)
     data = response.json()
