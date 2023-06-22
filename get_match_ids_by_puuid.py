b146794870add723776a2f1859cff49acacb7d66
Nolan <meyer09@gmail.com>
2023-06-22 12:38:37 -0500

    updated API functions to use get_API_key

--- get_match_ids_by_puuid.py
+++ get_match_ids_by_puuid.py
@@ -1,14 +1,8 @@
 import requests
-<<<<<<< HEAD
 from get_API_key import get_API_key
 def get_match_ids_by_puuid(puu_id, start=0, count=3):
     base_url = 'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puu_id}/ids?start={start}&count={count}&api_key={API_key}'
     url = base_url.format(puu_id=puu_id, start=start, count=count, API_key=get_API_key())
-=======
-def get_match_ids_by_puuid(puu_id, API_key, start=0, count=3):
-    base_url = 'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puu_id}/ids?start={start}&count={count}&api_key={API_key}'
-    url = base_url.format(puu_id=puu_id, start=start, count=count, API_key=API_key)
->>>>>>> main
     
     response = requests.get(url)
     
