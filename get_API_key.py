a7e7b6c5b4fb03c6c888a73db52c5f6760fd1b78
Nolan <meyer09@gmail.com>
2023-08-01 19:50:14 -0500

    API_query to get match timeline and split into tables

--- get_API_key.py
+++ get_API_key.py
@@ -1,12 +1,15 @@
-def get_API_key():
-
-    file_path = "API-key.txt" 
+def get_API_key(file_path="API-key.txt"):
+    API_key = None
 
     try:
         with open(file_path, 'r') as file:
-            API_key = file.read()
+            API_key = file.read().strip()  # strip() to remove any leading/trailing whitespace
     except FileNotFoundError:
-        print("File not found.")
+        raise FileNotFoundError(f"The file {file_path} was not found.")
     except IOError:
-        print("Error reading the file.")
-    return API_key
+        raise IOError(f"There was an error reading the file {file_path}.")
+
+    if API_key:
+        return API_key
+    else:
+        raise ValueError("API Key is empty.")
