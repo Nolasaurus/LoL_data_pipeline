889c07048d44d025da71f7046ebb91ff4e2f00a4
Nolan <meyer09@gmail.com>
2023-08-14 21:12:28 -0500

    add skeleton for summoner.html and sql queries

--- front_end/app.py
+++ front_end/app.py
@@ -4,9 +4,11 @@ from src.get_match_by_match_id import get_match_by_match_id
 from src.extract_data_from_match import extract_data_from_match
 from src.upload_matches import get_and_insert_match
 from src.get_puuid_by_name import get_puuid_by_name
+from src.extract_and_plot_gold_over_time import extract_gold_data, plot_gold_data
+from src.connect_db import connect_db
+from flask import Flask
 
 app = Flask(__name__, static_url_path='', static_folder='static')
-app.secret_key = "supersecretkey"  # Needed for flash messages
 
 @app.route('/lookup', methods=['POST'])
 def lookup():
@@ -24,11 +26,39 @@ def get_match(match_id):
     match_details_json, match_timeline_json = get_match_by_match_id(match_id)
     match_details_df = extract_data_from_match(match_details_json)
     match_details = match_details_df.to_records(index=False)
-    return render_template('match.html', match_details=match_details, columns=match_details_df.columns)
+    gold_data = extract_gold_data(match_id)
+    gold_chart_base64 = plot_gold_data(gold_data)
+
+    return render_template('match.html', match_details=match_details, columns=match_details_df.columns, gold_chart_base64=gold_chart_base64)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/summoner/<summoner_name>')
+def summoner(summoner_name):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    # SQL query to retrieve the last 3 matches for the given summoner
+    query = """
+        SELECT * FROM matches
+        JOIN players ON matches.id = players.match_id
+        WHERE players.summoner_name = %s
+        LIMIT 3;
+    """
+    cursor.execute(query, (summoner_name,))
+    matches = cursor.fetchall()
+
+    # You'll need to process the results into a format that the template expects
+    # ...
+
+    return render_template('summoner.html', summoner_name=summoner_name, matches=matches)
+
+
+# @app.route('/champion/<champion_id>')
+
+# @app.route('/summoner/<summoner_name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
