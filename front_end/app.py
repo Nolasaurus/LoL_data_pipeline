ee44a0d1b42b0dfb277a02f51209beb4ba8e2192
Nolan <meyer09@gmail.com>
2023-08-15 20:52:43 -0500

    update test files and app to handle api_client

--- front_end/app.py
+++ front_end/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-from src.get_match_ids_by_puuid import get_match_ids_by_puuid
-from src.get_match_by_match_id import get_match_by_match_id
+from src.api_client import API_client
 from src.extract_data_from_match import extract_data_from_match
 from src.upload_matches import get_and_insert_match
-from src.get_puuid_by_name import get_puuid_by_name
 from src.extract_and_plot_gold_over_time import extract_gold_data, plot_gold_data
 from src.connect_db import connect_db
 
@@ -16,16 +14,16 @@ def index():
 @app.route('/summoner', methods=['POST'])
 def lookup():
     summoner_name = request.form['summoner_name'].strip()
-    puuid = get_puuid_by_name(summoner_name)
+    puuid = API_client().get_puuid_by_name(summoner_name)
     if puuid:
-        match_ids = get_match_ids_by_puuid(puuid)
+        match_ids = API_client().get_match_ids_by_puuid(puuid)
         matches_data = [get_match(match_id) for match_id in match_ids[:3]]
         return render_template('summoner.html', summoner_name=summoner_name, matches=matches_data)
     else:
         return "Summoner name not found or an error occurred", 400
 
 def get_match(match_id):
-    match_details_json, match_timeline_json = get_match_by_match_id(match_id)
+    match_details_json, match_timeline_json = API_client().get_match_by_match_id(match_id)
     match_details_df = extract_data_from_match(match_details_json)
     match_details = match_details_df.to_dict(orient='records')  # Convert DataFrame to list of dictionaries
     # gold_data = extract_gold_data(match_id)
--- src/api_client.py
+++ src/api_client.py
@@ -18,36 +18,27 @@ class API_client:
         except requests.exceptions.Timeout:
             print(f"Request timed out for url: {url}")
             return None
+        
+    def get_puuid_by_name(self, summoner_name):
+        summoner_name_encoded = summoner_name.strip().replace(" ", "%20") # remove leading/trailing whitespaces and encode space character
+        url = f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name_encoded}?api_key={self.api_key}'
+
+        response = self._make_request(url, timeout=5)
+        if response is not None:
+            return response['puuid']
+        else:
+            # Handle the case where the request fails
+            return None
+
+    
+    def get_match_ids_by_puuid(self, puu_id, start=0, count=20):
+        url = f'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puu_id}/ids?start={start}&count={count}&api_key={self.api_key}'
+        return self._make_request(url, timeout=5)
 
     def get_match_by_match_id(self, match_id):
         url = f'https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={self.api_key}'
         return self._make_request(url, timeout=5)
 
-    def get_match_ids_by_puuid(self, puu_id, start=0, count=20):
-        url = f'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puu_id}/ids?start={start}&count={count}&api_key={self.api_key}'
-        return self._make_request(url)
-
     def get_match_timeline(self, match_id):
         url = f'https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}/timeline?api_key={self.api_key}'
-        return self._make_request(url)
-
-    def get_puuid_by_name(self, summoner_name):
-        summoner_name_encoded = summoner_name.strip().replace(" ", "%20") # Remove leading/trailing whitespaces and encode space character
-        url = f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name_encoded}?api_key={self.api_key}'
-        response = self._make_request(url)
-        return response['puuid'] if response else None
-
-    def get_puuid_by_summon_id(self, summoner_id):
-        url = f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/{summoner_id}?api_key={self.api_key}'
-        response = self._make_request(url)
-        return response['puuid'] if response else None
-    
-    def get_summoner_ids_from_league_id(self, league_id):
-        url = f'https://na1.api.riotgames.com/lol/league/v4/leagues/{league_id}?api_key={self.api_key}'
-        response = self._make_request(url)
-        if response:
-            summoner_ids = [entry['summonerId'] for entry in response['entries']]
-            return summoner_ids
-        else:
-            return None
-
+        return self._make_request(url, timeout=5)
