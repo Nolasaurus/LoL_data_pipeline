bfa044e159fc6469bac8681927d4962c8bedea99
Nolan <meyer09@gmail.com>
2023-11-16 10:33:58 -0600

    fix loading .env file in Docker build

--- project_root/src/api_client.py
+++ project_root/src/api_client.py
@@ -1,11 +1,10 @@
 import os
 from dotenv import load_dotenv
 import requests
-import webbrowser
-import subprocess
-import sys
+import logging
+import logging
 
-
+logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class RateLimitExceededError(Exception):
     pass
@@ -13,26 +12,20 @@ class RateLimitExceededError(Exception):
 class APIKeyExpiredError(Exception):
     def __init__(self, message="API key has expired"):
         super().__init__(message)
-        self.open_browser()
 
-    def open_browser(self):
-        url = "https://developer.riotgames.com/login"
-        if sys.platform == "win32":
-            # If the script is running on native Windows Python, use webbrowser.
-            webbrowser.open(url)
-        else:
-            # If the script is running in WSL, use `cmd.exe /C start`.
-            try:
-                subprocess.run(['cmd.exe', '/C', 'start', url], check=True)
-            except subprocess.CalledProcessError as e:
-                print(f"Failed to open browser: {e}")
 
 class API_Client:
     def __init__(self):
-        load_dotenv()
+        load_dotenv('./.env', verbose=True) 
         self.api_key = os.environ.get("RIOT_API_KEY")
+        if not self.api_key:
+            logging.error("API key is not set in environment variables")
+            raise Exception("API key is not set in environment variables")
+        logging.info(f"API key loaded: {self.api_key}")
+
 
     def _make_request(self, url, timeout=None):
+        logging.debug(f"Making request to URL: {url} with API key: {self.api_key}")
         # Common code to make an HTTP request to the API
         try:
             response = requests.get(url, timeout=timeout)
@@ -40,14 +33,18 @@ class API_Client:
             if response.status_code == 200:
                 return response.json()
             elif response.status_code == 403:
-                raise APIKeyExpiredError("Riot API key expired")
+                print(response)
+                raise APIKeyExpiredError("403: No Riot API key or key expired")
             elif response.status_code == 429:
-                raise RateLimitExceededError("Too many API calls recently")
+                raise RateLimitExceededError("429: Rate limited. Too many API calls recently")
             else:
                 print(f"Error: {response.status_code}")
                 return None
         except requests.exceptions.Timeout:
             print(f"Request timed out for url: {url}")
+            return None
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Request exception: {e}")
             return None
 
     def get_summoner_by_name(self, summoner_name):
@@ -58,8 +55,8 @@ class API_Client:
         response = self._make_request(url, timeout=5)
         return response if response else None
 
-    def get_match_ids_by_puuid(self, puu_id, start=0, count=20):
-        url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puu_id}/ids?start={start}&count={count}&api_key={self.api_key}"
+    def get_match_ids_by_puuid(self, puuid, start=0, count=20):
+        url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start={start}&count={count}&api_key={self.api_key}"
         return self._make_request(url, timeout=5)
 
     def get_match_by_match_id(self, match_id):
@@ -87,3 +84,7 @@ class API_Client:
             return summoner_ids
         else:
             return None
+
+
+if __name__ == "__main__":
+    client = API_Client()
