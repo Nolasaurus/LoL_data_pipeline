3860fdbb6d108bf2f6442964bf3458d3fcbee8f1
Nolan <meyer09@gmail.com>
2023-11-16 12:01:55 -0600

    format files with black

--- project_root/src/MatchOverclass.py
+++ project_root/src/MatchOverclass.py
@@ -3,6 +3,8 @@ from MatchDto import MatchDto
 from MatchTimelineDto import MatchTimelineDto
 from api_client import API_Client
 import pandas as pd
+import postgres_helperfile
+
 
 class MatchOverclass:
     """
@@ -22,34 +24,18 @@ class MatchOverclass:
         self.match_data = self.get_match_dto(match_id)
         self.match_timeline = self.get_match_timeline_dto(match_id)
         self.match_id = self.match_data.metadata.matchId
-        self.puuid_dict = {participant.puuid: participant.summonerName for participant in self.match_data.info.participants}
-
-    # def extract_match_summary(self):
-    #     # extract from match_data game start, game duration, game type, teams, winning team
-    #     game_start = self.match_data.info.gameStartTimestamp
-    #     UTC_game_start = None # convert game_start to UTC
-
-    #     game_duration = self.match_data.info.gameDuration
-    #     game_len_hh_mm_ss = None # convert game_duration to hours, minutes, seconds
-    #     # extract from match_timeline
-    #     match_summary_df = None
-
-    #     player_data = self.extract_player_data()
-    #     # get KDA for each participant, gold and level, champion, total damage to champions
-    #     return match_summary_df
-    
-    # def extract_player_data(self):
-    #     # convert match_data.info.participants to a df. Challenges and perks should each be their own df
-    #     ...
-
+        self.puuid_dict = {
+            participant.puuid: participant.summonerName
+            for participant in self.match_data.info.participants
+        }
 
     def get_gold_by_summoner_name(self):
         # Initialize an empty list to store the data
         gold_data = []
         participant_dict = {}
         for participant in self.match_timeline.info.participants:
-            participant_id = participant['participantId']
-            puuid = participant['puuid']
+            participant_id = participant["participantId"]
+            puuid = participant["puuid"]
             participant_dict[participant_id] = puuid
 
         for frame in self.match_timeline.info.frames:
@@ -65,10 +51,11 @@ class MatchOverclass:
                 gold_data.append([timestamp, summoner_name, total_gold])
 
         # Convert the list to a DataFrame
-        gold_df = pd.DataFrame(gold_data, columns=["frame", "summoner_name", "total_gold"])
+        gold_df = pd.DataFrame(
+            gold_data, columns=["frame", "summoner_name", "total_gold"]
+        )
 
         return gold_df
-
 
     def get_events_by_frame(self):
         event_data = []
@@ -97,22 +84,22 @@ class MatchOverclass:
 
         fig, ax = plt.subplots(figsize=(10, 6))
 
-        for summoner_name, group in gold_data.groupby('summoner_name'):
-            ax.plot(group['frame'], group['total_gold'], label=summoner_name)
+        for summoner_name, group in gold_data.groupby("summoner_name"):
+            ax.plot(group["frame"], group["total_gold"], label=summoner_name)
 
-        ax.set_xlabel('Frame')
-        ax.set_ylabel('Total Gold')
-        ax.set_title(f'Total Gold Over Time for Match {self.match_id}')
+        ax.set_xlabel("Frame")
+        ax.set_ylabel("Total Gold")
+        ax.set_title(f"Total Gold Over Time for Match {self.match_id}")
         ax.legend()
 
         return fig
-    
+
     @staticmethod
     def get_match_timeline_dto(match_id):
         api_client = API_Client()
         match_timeline = api_client.get_match_timeline(match_id)
         return MatchTimelineDto(match_timeline)
-    
+
     @staticmethod
     def get_match_dto(match_id):
         api_client = API_Client()
--- project_root/src/MatchTimelineDto.py
+++ project_root/src/MatchTimelineDto.py
@@ -1,8 +1,8 @@
 class Metadata:
     def __init__(self, metadata_json):
-        self.dataVersion = metadata_json['dataVersion']
-        self.matchId = metadata_json['matchId']
-        self.participants = metadata_json['participants']
+        self.dataVersion = metadata_json["dataVersion"]
+        self.matchId = metadata_json["matchId"]
+        self.participants = metadata_json["participants"]
 
 
 class ChampionStats:
@@ -19,12 +19,12 @@ class DamageStats:
 
 class ParticipantFrame:
     def __init__(self, p_frame_json):
-        self.championStats = ChampionStats(p_frame_json['championStats'])
-        self.damageStats = DamageStats(p_frame_json['damageStats'])
-        self.currentGold = p_frame_json['currentGold']
-        self.level = p_frame_json['level']
-        self.totalGold = p_frame_json['totalGold']
-        self.xp = p_frame_json['xp']
+        self.championStats = ChampionStats(p_frame_json["championStats"])
+        self.damageStats = DamageStats(p_frame_json["damageStats"])
+        self.currentGold = p_frame_json["currentGold"]
+        self.level = p_frame_json["level"]
+        self.totalGold = p_frame_json["totalGold"]
+        self.xp = p_frame_json["xp"]
 
 
 class Event:
@@ -32,24 +32,26 @@ class Event:
         for key, value in event_json.items():
             setattr(self, key, value)
 
+
 class Frame:
     def __init__(self, frame_json):
-        self.events = [Event(event) for event in frame_json['events']]
-        self.participantFrames = {k: ParticipantFrame(v) for k, v in frame_json['participantFrames'].items()}
-        self.timestamp = frame_json['timestamp']
+        self.events = [Event(event) for event in frame_json["events"]]
+        self.participantFrames = {
+            k: ParticipantFrame(v) for k, v in frame_json["participantFrames"].items()
+        }
+        self.timestamp = frame_json["timestamp"]
 
 
 class Info:
     def __init__(self, info_json):
-        self.frameInterval = info_json['frameInterval']
-        self.frames = [Frame(frame) for frame in info_json['frames']]
-        self.gameId = info_json['gameId']
-        self.participants = info_json['participants']
+        self.frameInterval = info_json["frameInterval"]
+        self.frames = [Frame(frame) for frame in info_json["frames"]]
+        self.gameId = info_json["gameId"]
+        self.participants = info_json["participants"]
 
 
 class MatchTimelineDto:
     def __init__(self, timeline_json):
-        self.metadata = Metadata(timeline_json['metadata'])
-        self.info = Info(timeline_json['info'])
+        self.metadata = Metadata(timeline_json["metadata"])
+        self.info = Info(timeline_json["info"])
         self.participants = self.metadata.participants
-
