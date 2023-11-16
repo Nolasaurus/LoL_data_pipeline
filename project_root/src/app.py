189b756990a722be5598c51a00b64832ddf34b2d
Nolan <meyer09@gmail.com>
2023-11-16 12:02:29 -0600

    format files with black

--- project_root/src/app.py
+++ project_root/src/app.py
@@ -6,26 +6,29 @@ from api_client import API_Client
 
 client = API_Client()
 
+
 def main():
     st.title("League of Legends Summoner Lookup")
-    summoner_name = st.text_input("Enter summoner name", placeholder="Enter summoner name")
+    summoner_name = st.text_input(
+        "Enter summoner name", placeholder="Enter summoner name"
+    )
 
     if summoner_name:
-        try: # API call to retrieve puuid from summoner name
+        try:  # API call to retrieve puuid from summoner name
             summoner_dto = SummonerDto.get_summoner_dto(summoner_name)
             summoner_puuid = summoner_dto.puuid
         except Exception as e:
             st.error(f"Error fetching summoner data: {e}")
             return
 
-        try: # API call to retrieve puuid's last 20 matches
+        try:  # API call to retrieve puuid's last 20 matches
             match_ids = client.get_match_ids_by_puuid(summoner_puuid)
             most_recent_match_id = match_ids[0]
         except Exception as e:
             st.error(f"Error fetching match IDs: {e}")
             return
 
-        try: # create MatchOverclass for match_id
+        try:  # create MatchOverclass for match_id
             combined_match_data = MatchOverclass(most_recent_match_id)
             fig = combined_match_data.plot_gold_by_frame()
             st.pyplot(fig)
