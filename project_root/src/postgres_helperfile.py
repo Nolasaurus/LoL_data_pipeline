405cc6ecd897acd1215282d379bd8866634ec541
Nolan <meyer09@gmail.com>
2023-11-27 09:07:41 -0600

    create helperfile for CRUD ops in postgres

--- project_root/src/postgres_helperfile.py
+++ project_root/src/postgres_helperfile.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+import sys
 from dotenv import load_dotenv
 
 
@@ -9,27 +10,66 @@ def connect_db():
         dbname="loldb",
         user="nolan",
         password=os.environ.get("POSTGRES_PASSWORD"),
-        host="localhost",
+        host="postgres",
         port="5432",
     )
 
 
-def create_table(table_name, columns):
-    with connect_db() as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                """
-                CREATE TABLE IF NOT EXISTS match (
-                    match_id TEXT PRIMARY KEY,
-                    match JSON NOT NULL,
-                    match_timeline JSON
-                );
-                """
-            )
-            conn.commit()
+def tables_exist(conn, table_names):
+    cursor = conn.cursor()
+    try:
+        for table_name in table_names:
+            cursor.execute(f"SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = '{table_name}');")
+            print(cursor.fetchall)
+            if not cursor.fetchone()[0]:
+                return False
+        return True
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        conn.rollback()
 
 
-def add_rows_to_table(table_name, data_df):
+def create_table_from_sql_file(sql_file_path, conn):
+    # Connect to the database
+    cursor = conn.cursor()
+
+    # Read SQL from file
+    with open(sql_file_path, 'r') as file:
+        sql_script = file.read()
+
+    # Execute SQL script
+    try:
+        cursor.execute(sql_script)
+        conn.commit()
+        print("Table created successfully")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def create_tables_if_none():
+    filepath = './sql_tables/'
+    sql_files_to_table_names = {
+        'match_metadata.sql': 'match_metadata',
+        'perks.sql': 'perks',
+        'player_match_data.sql': 'player_match_data',
+        'challenges.sql': 'challenges',
+        'participant_frames.sql': 'participant_frames',
+        'match_events.sql': 'match_events'
+    }
+
+    # Connect to the database
+    conn = connect_db()
+
+    # Check if tables exist and create them if they don't
+    for sql_file, table_name in sql_files_to_table_names.items():
+        if not tables_exist(conn, [table_name]):
+            create_table_from_sql_file(filepath + sql_file, conn)
+            print(f'created table: {table_name}')
+
+    # Close the database connection
+    conn.close()
+
+
+def add_df_to_table(table_name, data_df):
     conn = connect_db()
     cursor = conn.cursor()
     try:
@@ -61,4 +101,34 @@ def add_rows_to_table(table_name, data_df):
         print("An error occurred:", e)
 
 
+def add_dict_to_table(table_name, data_dict):
+    conn = connect_db()
+    cursor = conn.cursor()
+    try:
+        # Retrieve the columns from the table in the database
+        cursor.execute(f"SELECT * FROM {table_name} LIMIT 0")
+        db_columns = [desc[0] for desc in cursor.description]
 
+        # Check if the dictionary keys match the database columns
+        if set(db_columns) != set(data_dict.keys()):
+            print("Keys in dict do not match cols in db table. Aborted.")
+            return
+
+        # Create the INSERT INTO statement
+        columns = ', '.join(data_dict.keys())
+        placeholders = ', '.join(['%s'] * len(data_dict))
+        insert_stmt = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+
+        # Execute the insert
+        cursor.execute(insert_stmt, list(data_dict.values()))
+        conn.commit()
+        
+    except Exception as e:
+        # If any errors occur, rollback the transaction
+        conn.rollback()
+        print("An error occurred:", e)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2 and sys.argv[1] == "create_table_from_sql_file":
+        create_table_from_sql_file(sys.argv[2])
