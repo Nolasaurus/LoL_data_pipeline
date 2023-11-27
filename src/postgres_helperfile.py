35f1ed0fb439609e1770eb4d1877e7cbae88b21b
Nolan <meyer09@gmail.com>
2023-11-27 15:59:14 -0600

    add readonly connection for psql

--- src/postgres_helperfile.py
+++ src/postgres_helperfile.py
@@ -1,94 +1,78 @@
 import os
 import psycopg2
-import sys
 from dotenv import load_dotenv
 
-def connect_db():
+def connect_db(user_role='admin'):
     load_dotenv()
+    user = 'nolan' if user_role == 'admin' else 'read_only'
+    password_key = 'POSTGRES_PASSWORD' if user_role == 'admin' else 'READONLY_POSTGRES_PASSWORD'
+    
     return psycopg2.connect(
         dbname="loldb",
-        user="nolan",
-        password=os.environ.get("POSTGRES_PASSWORD"),
+        user=user,
+        password=os.environ.get(password_key),
         host="postgres",
         port="5432",
     )
 
+def create_read_only_user(username, password):
+    with connect_db('admin') as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("CREATE USER %s WITH PASSWORD %s;", (username, password))
+            cursor.execute("GRANT CONNECT ON DATABASE loldb TO %s;", (username,))
+            cursor.execute("GRANT USAGE ON SCHEMA public TO %s;", (username,))
+            cursor.execute("GRANT SELECT ON ALL TABLES IN SCHEMA public TO %s;", (username,))
+            cursor.execute("ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO %s;", (username,))
+
+
 def execute_sql_file(file_path):
     with open(file_path, 'r') as file:
         sql_script = file.read()
-        connect_db().cursor.execute(sql_script)
+        with connect_db('admin') as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(sql_script)
 
 def tables_exist(table_names):
-    conn = connect_db()
-    cursor = conn.cursor()
-    try:
-        for table_name in table_names:
-            cursor.execute(f"SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = '{table_name}');")
-            print(cursor.fetchall)
-            if not cursor.fetchone()[0]:
-                return False
-        return True
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        conn.rollback()
+    with connect_db('read_only') as conn:
+        with conn.cursor() as cursor:
+            for table_name in table_names:
+                cursor.execute("SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = %s);", (table_name,))
+                if not cursor.fetchone()[0]:
+                    return False
+            return True
 
 def add_df_to_table(table_name, data_df):
-    conn = connect_db()
-    cursor = conn.cursor()
-    try:
-        # Retrieve the columns from the table in the database
-        cursor.execute(f"SELECT * FROM {table_name} LIMIT 0")
-        db_columns = [desc[0] for desc in cursor.description]
+    with connect_db() as conn:
+        with conn.cursor('admin') as cursor:
+            cursor.execute(f"SELECT * FROM {table_name} LIMIT 0")
+            db_columns = [desc[0] for desc in cursor.description]
+            df_columns = data_df.columns.tolist()
 
-        # Get the columns from the DataFrame
-        df_columns = data_df.columns.tolist()
+            if db_columns != df_columns:
+                print("Columns in df do not match cols in db table. Aborted.")
+                return
 
-        # Check if the columns match
-        if db_columns != df_columns:
-            print("Columns in df do not match cols in db table. Aborted.")
-            return
+            values = [tuple(row) for row in data_df.values]
+            insert_stmt = f"INSERT INTO {table_name} ({','.join(df_columns)}) VALUES %s"
+            psycopg2.extras.execute_values(cursor, insert_stmt, values)
 
-        values = [tuple(row) for row in data_df.values]
-
-        # Create the INSERT INTO statement
-        insert_stmt = f"INSERT INTO {table_name} ({','.join(df_columns)}) VALUES %s"
-
-        # Execute the insert
-        psycopg2.extras.execute_values(cursor, insert_stmt, values)
-
-        # Commit the transaction
-        conn.commit()
-    except Exception as e:
-        # If any errors occur, rollback the transaction
-        conn.rollback()
-        print("An error occurred:", e)
 
 def add_dict_to_table(table_name, data_dict):
-    conn = connect_db()
-    cursor = conn.cursor()
-    try:
-        # Retrieve the columns from the table in the database
-        cursor.execute(f"SELECT * FROM {table_name} LIMIT 0")
-        db_columns = [desc[0] for desc in cursor.description]
+    with connect_db('admin') as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM {table_name} LIMIT 0")
+            db_columns = [desc[0] for desc in cursor.description]
 
-        # Check if the dictionary keys match the database columns
-        if set(db_columns) != set(data_dict.keys()):
-            print("Keys in dict do not match cols in db table. Aborted.")
-            return
+            if set(db_columns) != set(data_dict.keys()):
+                print("Keys in dict do not match cols in db table. Aborted.")
+                return
 
-        # Create the INSERT INTO statement
-        columns = ', '.join(data_dict.keys())
-        placeholders = ', '.join(['%s'] * len(data_dict))
-        insert_stmt = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+            columns = ', '.join(data_dict.keys())
+            placeholders = ', '.join(['%s'] * len(data_dict))
+            insert_stmt = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+            cursor.execute(insert_stmt, list(data_dict.values()))
 
-        # Execute the insert
-        cursor.execute(insert_stmt, list(data_dict.values()))
-        conn.commit()
-        
-    except Exception as e:
-        # If any errors occur, rollback the transaction
-        conn.rollback()
-        print("An error occurred:", e)
+    
 
 # def _create_initial_tables():
 #     # SQL file paths in the required order
@@ -123,7 +107,3 @@ def add_dict_to_table(table_name, data_dict):
 #             cursor.close()
 #         if conn:
 #             conn.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) > 2 and sys.argv[1] == "execute_sql_file":
-        execute_sql_file(sys.argv[2])
