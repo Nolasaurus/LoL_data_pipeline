a6e9157cfaa0639f6ec7347e8c967762c53acf13
Nolan <meyer09@gmail.com>
2024-01-08 22:31:48 -0600

    add exception handling, black formatting, change logging level

--- src/app.py
+++ src/app.py
@@ -1,39 +1,46 @@
+import logging
 import streamlit as st
 import psycopg2
+import pandas as pd
+
 from api_client import API_Client
 import postgres_helperfile
-from extract_data import insert_match
+from insert_data import insert_match
 from match_classes import SummonerDto
-import pandas as pd
-import logging
 
-# Set up logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 # postgres admin
-ACCOUNT_IS_ADMIN = True
+ACCOUNT_IS_ADMIN = False
+
 
 # NA1_4864820958
 def main():
     st.title("League of Legends Summoner Lookup")
 
-    summoner_name = st.text_input("Enter summoner name", placeholder="Enter summoner name")
+    summoner_name = st.text_input(
+        "Enter summoner name", placeholder="Enter summoner name"
+    )
     if summoner_name:
         try:
-            logging.info(f"Fetching data for summoner: {summoner_name}")
+            logging.info("Fetching data for summoner: %s", summoner_name)
             # API call to retrieve puuid from summoner names
             summoner_json = API_Client().get_summoner_by_name(summoner_name)
             summoner_dto = SummonerDto(summoner_json)
             summoner_puuid = summoner_dto.puuid
         except Exception as e:
-            logging.error(f"Error fetching summoner data: {e}")
+            logging.error("Error fetching summoner data: %s", e)
             st.error(f"Error fetching summoner data: {e}")
             return
 
         try:
             # API call to retrieve puuid's last 20 matches
             match_ids = API_Client().get_match_ids_by_puuid(summoner_puuid)
-            logging.info(f"Retrieved match IDs for {summoner_name}")
+            logging.info("Retrieved match IDs for %s", summoner_name)
             st.write(match_ids)
         except Exception as e:
             logging.error(f"Error fetching match IDs: {e}")
@@ -43,7 +50,15 @@ def main():
         for match_id in match_ids:
             logging.info(f"Processing match ID: {match_id}")
             match_dto = API_Client().get_match_by_match_id(match_id)
+            if match_dto:
+                st.write(f"match_dto success for {match_id}")
+
             match_timeline_dto = API_Client().get_match_timeline(match_id)
+            if match_timeline_dto:
+                st.write(f"match_timeline_dto success for {match_id}")
+
+            if match_dto is None or match_timeline_dto is None:
+                raise Exception("Riot API call failed. Check API key.")
 
             insert_match(match_dto, match_timeline_dto)
 
@@ -62,7 +77,9 @@ def main():
 
                 if user_query.strip().lower().startswith("select"):
                     rows = cursor.fetchall()
-                    df = pd.DataFrame(rows, columns=[desc[0] for desc in cursor.description])
+                    df = pd.DataFrame(
+                        rows, columns=[desc[0] for desc in cursor.description]
+                    )
                     st.dataframe(df)  # Display in tabular format
 
         except psycopg2.Error as e:
